0afdd60bfbf86958d2a2d705f0f0574b4a19c269
dlwnajr <chulsaniii@gmail.com>
2024-08-09 15:09:57 +0900

    feat:도로명 주소 기능 추가

--- src/pages/Mypage/setting.js
+++ src/pages/Mypage/setting.js
@@ -1,9 +1,16 @@
 import React, { useState, useRef } from 'react';
+import { useDaumPostcodePopup } from 'react-daum-postcode'; 
 import './setting.css';
 
 function SettingPage() {
   const [storeImage, setStoreImage] = useState(null);
+  const [addressObj, setAddressObj] = useState({
+    zipcode: '',          // 우편번호 필드 추가
+    areaAddress: '',
+    townAddress: ''
+  });
   const logoImgInput = useRef(null);
+  const postcodeScriptUrl = "https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js";
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -19,6 +26,35 @@ function SettingPage() {
   const triggerFileInput = () => {
     logoImgInput.current.click();
   };
+
+  const open = useDaumPostcodePopup(postcodeScriptUrl);
+
+  const handleComplete = (data) => {
+    let fullAddress = data.address;
+    let extraAddress = ''; //추가될 주소
+    let localAddress = data.sido + ' ' + data.sigungu; //지역주소(시, 도 + 시, 군, 구)
+    if (data.addressType === 'R') { //주소타입이 도로명주소일 경우
+      if (data.bname !== '') {
+        extraAddress += data.bname; //법정동, 법정리
+      }
+      if (data.buildingName !== '') { //건물명
+        extraAddress += (extraAddress !== '' ? `, ${data.buildingName}` : data.buildingName);
+      }
+      //지역주소 제외 전체주소 치환
+      fullAddress = fullAddress.replace(localAddress, '');
+      //조건 판단 완료 후 우편번호, 지역 주소 및 상세주소 state 수정
+      setAddressObj({
+        zipcode: data.zonecode, // 우편번호 추가
+        areaAddress: localAddress,
+        townAddress: fullAddress += (extraAddress !== '' ? `(${extraAddress})` : '')
+      });
+    }
+  }
+
+  const handleClick = () => {
+    //주소검색이 완료되고, 결과 주소를 클릭 시 해당 함수 수행
+  	open({onComplete: handleComplete});
+  }
 
   return (
     <div className="setting">
@@ -65,13 +101,27 @@ function SettingPage() {
       </div>
       <div className="Info-section">
         <div className="setting-title">주소지 정보</div>
-        <input type="text" placeholder="주소를 입력해주세요." />
-        <button className="StoreSet-button2">변경</button>
+        <input 
+          type="text"
+          className="zipcode-input"
+          placeholder="우편번호" 
+          value={addressObj.zipcode}  // 우편번호 표시
+          readOnly
+        />
+        <button className="StoreSet-button" onClick={handleClick}>주소 찾기</button>
+        <input 
+          type="text"
+          placeholder="주소를 입력해주세요." 
+          value={addressObj.areaAddress || addressObj.townAddress ? `${addressObj.areaAddress} ${addressObj.townAddress}` : ''}
+          onChange={(e) => {}}
+          readOnly
+        />
+        <input type="text" placeholder="상세 주소를 입력해주세요." />
       </div>
       <div className="Info-section2">
         <div className="setting-title">계좌 정보</div>
         <input type="text" placeholder="계좌 정보를 입력해주세요." />
-        <button className="StoreSet-button2">변경</button>
+        <button className="StoreSet-button">변경</button>
       </div>
     </div>
   );
