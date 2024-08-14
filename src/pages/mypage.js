1a335c554cb4ddf25cc70050f9c5d3646a4054d5
dlwnajr <sh>
2024-08-14 12:57:33 +0900

    feat:상품상세페이지 기능 수정

--- src/pages/mypage.js
+++ src/pages/mypage.js
@@ -7,11 +7,11 @@ function MyPage() {
     <div className="mypage">
       <nav className="sidebar">
         <div className="sidebar-title">마이페이지</div>
-        <NavLink to="/mypage" className="sidebar-con" end>내 정보 / 상점 관리</NavLink>
-        <NavLink to="/mypage/purchased" className="sidebar-con">구매 내역</NavLink>
-        <NavLink to="/mypage/sales" className="sidebar-con">판매 내역</NavLink>
-        <NavLink to="/mypage/fav" className="sidebar-con">관심</NavLink>
-        <NavLink to="/mypage/talk" className="sidebar-con">채팅</NavLink>
+        <NavLink to="/mypage" className="sidebar-header" end>내 정보 / 상점 관리</NavLink>
+        <NavLink to="/mypage/purchased" className="sidebar-header">구매 내역</NavLink>
+        <NavLink to="/mypage/sales" className="sidebar-header">판매 내역</NavLink>
+        <NavLink to="/mypage/fav" className="sidebar-header">관심</NavLink>
+        <NavLink to="/mypage/talk" className="sidebar-header">채팅</NavLink>
       </nav>
       <div className="content">
         <Outlet /> {/* 자식페이지 */}
